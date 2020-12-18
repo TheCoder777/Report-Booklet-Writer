fc50257a4a0c80d5080ab5792eed1b016d382026
TheCoder777 <thecoder777.github@gmail.com>
2020-12-18 14:15:22 +0100

    removed some licenses where it was overkill, spaced out some docstrings

--- rbwriter/wsgi.py
+++ rbwriter/wsgi.py
@@ -2,8 +2,10 @@ from rbwriter import create_app
 
 """
 This file provides an interface to the uwsgi module.
+
 The uwsgi module would normally read the 'Flask(__name__)' or 'app' instance directly from the module itself.
 However, this isn't possible due to the use of the flask app_factory in this project.
+
 To make it still work, this interface imports the app_factory and creates a application object (alias to 'app').
 Note that this is only for running from the rbwriter script!
 """
